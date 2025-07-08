70b7d89156ce6905a2b616751a7c531b8a3e4326
BynJerL <yggcphanq@gmail.com>
2025-07-08 16:00:24 +0700

    Implement Character class and player instances

--- gameScript.js
+++ gameScript.js
@@ -1,1 +1,13 @@
-console.log("Hello!")
+class Character {
+    /** @param {string} name @param {number} health @param {number} attackPower  */ 
+    constructor(name, health, attackPower) {
+        this.name = name;
+        this.health = health;
+        this.attackPower = attackPower;
+    }
+}
+
+const players = [
+    new Character("Hero", 100, 20),
+    new Character("Villain", 80, 15)
+];
