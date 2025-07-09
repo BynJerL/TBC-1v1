0d731a2c0ca8488d65cd789330cbb0492351e11b
BynJerL <yggcphanq@gmail.com>
2025-07-09 12:15:26 +0700

    Refactor Character class constructor and attack method for improved parameter handling and damage calculation

--- gameScript.js
+++ gameScript.js
@@ -1,13 +1,22 @@
 class Character {
-    /** @param {string} name @param {number} health @param {number} attackPower @param {number} defensePower  */ 
+    /** 
+     * @param {string} name @param {number} health 
+     * @param {number} attackPower @param {number} defensePower  
+     * */ 
     constructor(name, health, attackPower, defensePower, critChance = 0.1, critDamage = 0.5) {
         this.name = name;
         this.health = health;
         this.maxHealth = health;
         this.attackPower = attackPower;
         this.defensePower = defensePower;
+        this.fireResistance = null;
+        this.waterResistance = null;
+        this.earthResistance = null;
+        this.windResistance = null;
+        this.lightningResistance = null;
         this.critChance = critChance;
         this.critDamage = critDamage;
+        this.isGuard = false;
     }
 
     /** @param {number} amount*/ 
@@ -23,7 +32,7 @@ class Character {
     /** @param {Character} target @returns {number} */
     attack (target) {
         const isCrit = Math.random() < 0.1;
-        const damage = Math.max(0, Math.floor(this.attackPower * (1 + isCrit * this.critDamage) - target.defensePower * 0.5));
+        const damage = Math.max(0, Math.floor(this.attackPower * (1 + isCrit * this.critDamage) - (target.defensePower * 0.2 + target.isGuard * target.defensePower * 0.6)));
         target.takeDamage(damage);
         return damage;
     } 
@@ -38,8 +47,8 @@ class Character {
 }
 
 const players = [
-    new Character("Hero", 300, 30, 100),
-    new Character("Villain", 150, 15, 5)
+    new Character("Hero", 500, 30, 120),
+    new Character("Villain", 1000, 50, 0)
 ];
 
 const GameManager = {
